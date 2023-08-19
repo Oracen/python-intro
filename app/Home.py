4cc6126d3ad5d667cf52d0e86d9615f4e0ff158c
Oracen <alex.stenlake@gmail.com>
2023-08-19 21:42:54 +1000

    Add code health scripting, wireframed program layout and demo app

---
+++ app/Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Welcome to the Demo Club!")
+
+st.markdown(
+    "Congrats on making it this far! Here's a small reward for all your hard work "
+    "getting this far - a bit of playtime before we get back to learning."
+)
+
+st.markdown(
+    "This demo is a machine learning system live on your machine, being "
+    "recalculated in response to your inputs. Have a play around with it, and "
+    "later we'll explore how to build this site (on branch feature/simple-app), "
+    "as well as give you a more complicated demo to play with (on branch "
+    "feature/complex-app)."
+)
