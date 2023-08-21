5513da966392e66e9484ff723837ae1127d96a0e
Oracen <alex.stenlake@gmail.com>
2023-08-21 21:34:48 +1000

    fix: fix profile searching for pyenv before .bashrc can be read

--- bootstrap/bash-pyenv-profile.sh
+++ bootstrap/bash-pyenv-profile.sh
@@ -1,15 +1,12 @@
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init -)"' >> ~/.profile
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bash_profile
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.profile
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 
 echo "Shell environment set up, exit to force restart"
