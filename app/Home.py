9bf22668ede5ffbebaa797b86ec22128835321b3
Oracen <alex.stenlake@gmail.com>
2023-09-16 15:04:48 +1000

    fix: Alter simple_app to a basic skeleton ready for the workshop

--- app/Home.py
+++ app/Home.py
@@ -1,16 +1,5 @@
 import streamlit as st
 
-st.title("Welcome to the Demo Club!")
+st.title("Watch this space!")
 
-st.markdown(
-    "Congrats on making it this far! Here's a small reward for all your hard work "
-    "getting this far - a bit of playtime before we get back to learning."
-)
-
-st.markdown(
-    "This demo is a machine learning system live on your machine, being "
-    "recalculated in response to your inputs. Have a play around with it, and "
-    "later we'll explore how to build this site (on branch feature/simple-app), "
-    "as well as give you a more complicated demo to play with (on branch "
-    "feature/complex-app)."
-)
+st.markdown("Welcome to the simple app! Let's build this up together!")
