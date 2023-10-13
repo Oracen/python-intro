3c313d364989db73f6f8d585a9a9c0efda09ffd0
D Tim Cummings <tim@triptera.com.au>
2023-10-13 17:23:01 +1000

    Add bootstrap specific to pyenv-win. Fix typos

---
+++ bootstrap/bash-pyenv-win-profile.sh
@@ -0,0 +1,13 @@
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bash_profile
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+
+echo 'export PATH=$HOME/AppData/Roaming/Python/Scripts:$PATH' >> ~/.bash_profile
+echo 'export PATH=$HOME/AppData/Roaming/Python/Scripts:$PATH' >> ~/.bashrc
+
+echo "Shell environment set up, exit to force restart"
